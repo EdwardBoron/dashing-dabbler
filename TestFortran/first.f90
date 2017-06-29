7c2c1c995a8b3faec6685b2670322da077a093bc
EdwardBoron <EdwardBoron@users.noreply.github.com>
2017-06-29 23:42:35 +0200

    TestFortran/

---
+++ TestFortran/first.f90
@@ -0,0 +1,7 @@
+program hello
+!!!----------------------nonsense bar-----------------------!!!
+          print *, "Hello World!"
+          print *, "AAAAAAAAAAAAAAAH!"
+          print *, "My first FORTRAN program :)"
+!!!---------------------aaaaaand another one----------------!!!
+       end program hello
